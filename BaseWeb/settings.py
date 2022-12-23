2d02aa59c943a19427222a789c658f3a721a1c0b
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-23 18:43:33 +0700

    fix: flake err

--- BaseWeb/settings.py
+++ BaseWeb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','yota.site']
+ALLOWED_HOSTS = ['127.0.0.1', 'yota.site']
 
 
 # Application definition
