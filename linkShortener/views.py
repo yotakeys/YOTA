82dc58bc9178de3543141c81b1d124ee7fc57821
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-26 13:37:14 +0700

    fix: space in url (link shortener)

--- linkShortener/views.py
+++ linkShortener/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
-
+from django import forms
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
