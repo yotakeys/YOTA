e0695609ca1cc078bddd1a3ce709b1b3b5979f1e
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-26 11:40:16 +0700

    fix: versioning and debug

--- linkShortener/migrations/0001_initial.py
+++ linkShortener/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             name='Url',
             fields=[
                 ('longUrl', models.TextField()),
-                ('shortUrl', models.CharField(max_length=30, primary_key=True, serialize=False)), # noqa: E501
+                ('shortUrl', models.CharField(max_length=30, primary_key=True, serialize=False)),  # noqa: E501
                 ('create', models.DateTimeField(auto_now_add=True)),
                 ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),  # noqa: E501
             ],
