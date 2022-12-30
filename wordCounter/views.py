412051dbf472e7efd5a9eca46bbf6000e697073b
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-30 21:39:31 +0700

    fix: ver, debug, flake (3)

--- wordCounter/views.py
+++ wordCounter/views.py
@@ -26,11 +26,11 @@ def wordCounter(request):
         __sentences = len(__text.split('.')) - 1
         __paragraphs = len(__text.split('\n'))
 
-        __readingTime = __words/4.0
-        __readingTimeMinute = floor(__readingTime/60)
-        __readingTimeSecond = floor(__readingTime%60)
+        __readingTime = __words / 4.0
+        __readingTimeMinute = floor(__readingTime / 60)
+        __readingTimeSecond = floor(__readingTime % 60)
 
-    return render(request, 'wordCounter/index.html', context = {
+    return render(request, 'wordCounter/index.html', context={
         'text': __textPass,
         'char': __characters,
         'word': __words,
