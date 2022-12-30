71d6de17b270a69d1d8ba31ee2ae880a0aea238d
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-30 21:28:53 +0700

    feat: add testing word counter app

--- wordCounter/test_file.py
+++ wordCounter/test_file.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# import pytest
+from django.test import Client
+from django.urls import reverse, resolve
 
-# Create your tests here.
+def test_urllist_view():
+    client = Client()
+    response = client.get('/wordcounter/')
+    assert response.status_code == 200
+
+
+def test_urllist_url():
+    path = reverse('wordcounter')
+    assert resolve(path).view_name == "wordcounter"
