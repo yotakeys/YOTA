725c31006ecd65048f37ee7def9b47c29a03cd98
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-01-24 22:03:39 +0700

    feat: add templates

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from .models import Post
 
 class PostList(ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'blog/post_list.html'
 
 class PostDetail(DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
