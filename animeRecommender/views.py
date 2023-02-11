21ff228355cdfadb9131d6171ac0479176f92544
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-02-11 23:15:24 +0700

    feat: add functionality

--- animeRecommender/views.py
+++ animeRecommender/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+import requests
 
 # Create your views here.
 def animeRecommender(request):
-    return HttpResponse("Hello world")
+    
+    data = []
+    amount = 1
+    description = ""
+    
+    if request.method == 'POST':
+        
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+        
+        response = requests.post("https://yotakey-anime-recommendation-based-descr-a345a91.hf.space/run/predict", json={
+        "data": [
+            description,
+            amount,
+            ]
+        }).json()
+
+        data = response["data"][0].split(",\n")[:-1]
+    
+    return render(request, 'animeRecommender/index.html', context={'datas':data, 'amount':amount, 'description' : description})
